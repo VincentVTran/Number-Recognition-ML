e9a74d9d30442dd61947831431507bf29e525b76
Vincent Tran <Vincent.Tran@jbhunt.com>
2019-07-16 15:55:55 -0500

    Prepared data to be divided into four segments

--- recognitionApp/neuralNetwork.js
+++ recognitionApp/neuralNetwork.js
@@ -1,6 +1,19 @@
 const tf = require('@tensorflow/tfjs');
+var express = require('express')
 const imageDriver = require('./processImage');
 
-var imagePath = 'C:/Users/jisavt5/Downloads/result.png';
-//console.log(imageDriver.processImage(imagePath));
 
+var imagePath = 'C:/Users/jisavt5/Downloads/result.png';
+var originalInputSize = 0;
+imageDriver.processImage(imagePath).then(result => {originalInputSize = result.length});
+
+
+//ExpressJS 
+var app = express();
+const port = 3000;
+
+app.get('/', function (req, res) {
+    res.send('root')
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
