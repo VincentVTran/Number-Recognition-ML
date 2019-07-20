85b27e0763e6199797a90da06ce97e4176bb48a1
Vincent Tran <VincentTran197120@gmail.com>
2019-07-19 19:31:29 -0500

    Added a correction form into angular application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule, MatButtonModule, MatIconModule,MatCardModule } from '@angular/material';
+import { MatMenuModule, MatButtonModule, MatIconModule,MatCardModule,MatFormFieldModule,MatSelectModule} from '@angular/material';
 
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { MainScreenComponent } from './components/main-screen/main-screen.component';
 import { CanvasWhiteboardModule } from 'ng2-canvas-whiteboard';
 import { AboutComponent } from './components/about/about.component';
 import { CustomDetailComponent } from './components/custom-detail/custom-detail.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,6 +30,8 @@ import { CustomDetailComponent } from './components/custom-detail/custom-detail.
     MatMenuModule,
     MatIconModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
 
     //3rd-Party
     CanvasWhiteboardModule,
--- src/app/components/main-screen/main-screen.component.ts
+++ src/app/components/main-screen/main-screen.component.ts
@@ -8,14 +8,25 @@ import {CanvasWhiteboardComponent} from 'ng2-canvas-whiteboard';
 })
 export class MainScreenComponent implements OnInit {
   @ViewChild('canvasWhiteboard',{static: false}) canvasWhiteboard: CanvasWhiteboardComponent;
+ 
+  private predictedNumber:number = 0;
+  private correctHidden:boolean = true;
+  private buttonHidden:boolean = false;
 
   ngOnInit(){
-
   }
 
   onSubmit(){
     this.canvasWhiteboard.downloadCanvasImage("image/png","","result")
     this.canvasWhiteboard.clearCanvas();
+    this.buttonHidden = true;
+    this.correctHidden = false;
+    //console.log(this.canvasWhiteboard.context);
+  }
+
+  train(){
+    this.buttonHidden = false;
+    this.correctHidden = true;
     //console.log(this.canvasWhiteboard.context);
   }
 }
